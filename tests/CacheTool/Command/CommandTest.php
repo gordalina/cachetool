d42018b6b931f55806446c88c370b331de99d84f
gordalina <samuel.gordalina@gmail.com>
2014-12-12 17:16:32 +0000

    Use application with cli interface

--- tests/CacheTool/Command/CommandTest.php
+++ tests/CacheTool/Command/CommandTest.php
@@ -3,6 +3,7 @@
 namespace CacheTool\Command;
 
 use CacheTool\Console\Application;
+use CacheTool\Console\Config;
 use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\StreamOutput;
 
@@ -10,18 +11,16 @@ abstract class CommandTest extends \PHPUnit_Framework_TestCase
 {
     public function runCommand($cmd)
     {
-        $app = new Application;
+        $app = new Application(new Config(array('adapter' => 'cli')));
         $app->setAutoExit(false);
 
         $fp = fopen('php://memory', 'a+');
 
-        $input = new StringInput($cmdline);
+        $input = new StringInput($cmd);
         $output = new StreamOutput($fp);
 
         $app->run($input, $output);
 
-        fseek($fp, 0);
-
-        return stream_get_contents($fp);
+        return stream_get_contents($fp, -1, 0);
     }
 }

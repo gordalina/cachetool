aef196c1b2b0ad24b79cc16c5025e2d9107207e9
gordalina <samuel.gordalina@gmail.com>
2014-12-13 01:39:55 +0000

    Use BufferedOutput

--- tests/CacheTool/Command/CommandTest.php
+++ tests/CacheTool/Command/CommandTest.php
@@ -5,7 +5,7 @@ namespace CacheTool\Command;
 use CacheTool\Console\Application;
 use CacheTool\Console\Config;
 use Symfony\Component\Console\Input\StringInput;
-use Symfony\Component\Console\Output\StreamOutput;
+use Symfony\Component\Console\Output\BufferedOutput;
 
 abstract class CommandTest extends \PHPUnit_Framework_TestCase
 {
@@ -14,13 +14,11 @@ abstract class CommandTest extends \PHPUnit_Framework_TestCase
         $app = new Application(new Config(array('adapter' => 'cli')));
         $app->setAutoExit(false);
 
-        $fp = fopen('php://memory', 'a+');
-
         $input = new StringInput($cmd);
-        $output = new StreamOutput($fp);
+        $output = new BufferedOutput();
 
         $app->run($input, $output);
 
-        return stream_get_contents($fp, -1, 0);
+        return $output->fetch();
     }
 }

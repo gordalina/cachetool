a28c6fe99862101bf2b321340fc9e256bbe7cdae
gordalina <samuel.gordalina@gmail.com>
2014-12-13 02:55:23 +0000

    Use apc_exists

--- src/CacheTool/Command/ApcKeyExistsCommand.php
+++ src/CacheTool/Command/ApcKeyExistsCommand.php
@@ -12,6 +12,8 @@
 namespace CacheTool\Command;
 
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 class ApcKeyExistsCommand extends ApcKeyFetchCommand
 {
@@ -25,5 +27,23 @@ class ApcKeyExistsCommand extends ApcKeyFetchCommand
             ->setDescription('Checks if an APC key exists')
             ->addArgument('key', InputArgument::REQUIRED)
             ->setHelp('');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->ensureExtensionLoaded('apc');
+
+        $key = $input->getArgument('key');
+        $value = $this->getCacheTool()->apc_exists($key);
+
+        if ($value) {
+            $output->writeln("<comment>APC key=<info>{$key}</info> exists</comment>");
+        } else {
+            $output->writeln("<comment>APC key=<info>{$key}</info> does not exist.</comment>");
+            return 1;
+        }
     }
 }

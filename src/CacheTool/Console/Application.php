bfe79556ec1cc7091cc28e8ba583d302b77fea13
Ben Challis <ben@lendable.co.uk>
2018-10-25 17:29:32 +0100

    Fixed missing registration of OpcacheCompileScriptsCommand

--- src/CacheTool/Console/Application.php
+++ src/CacheTool/Console/Application.php
@@ -93,7 +93,7 @@ class Application extends BaseApplication
             $commands[] = new CacheToolCommand\OpcacheStatusCommand();
             $commands[] = new CacheToolCommand\OpcacheStatusScriptsCommand();
             $commands[] = new CacheToolCommand\OpcacheInvalidateScriptsCommand();
-            $commands[] = new CacheToolCommand\OpcacheInvalidateScriptsCommand();
+            $commands[] = new CacheToolCommand\OpcacheCompileScriptsCommand();
         }
 
         $commands[] = new CacheToolCommand\StatCacheClearCommand();

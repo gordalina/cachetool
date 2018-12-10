16bc1802456bfd4ae0e0538a6cc9473e0285eeb5
Gennadii Aleksandrov <Gennadii_Aleksandrov@epam.com>
2018-12-10 13:21:24 +0300

    Add opcache:compile:script command to compile a single file

--- src/CacheTool/Console/Application.php
+++ src/CacheTool/Console/Application.php
@@ -93,7 +93,8 @@ class Application extends BaseApplication
             $commands[] = new CacheToolCommand\OpcacheStatusCommand();
             $commands[] = new CacheToolCommand\OpcacheStatusScriptsCommand();
             $commands[] = new CacheToolCommand\OpcacheInvalidateScriptsCommand();
-            $commands[] = new CacheToolCommand\OpcacheInvalidateScriptsCommand();
+            $commands[] = new CacheToolCommand\OpcacheCompileScriptsCommand();
+            $commands[] = new CacheToolCommand\OpcacheCompileScriptCommand();
         }
 
         $commands[] = new CacheToolCommand\StatCacheClearCommand();

793742b6788b183999ab33c216c78615b9c9d7c8
Stephan Jorek <38257959+brandung-sjorek@users.noreply.github.com>
2019-08-08 19:16:59 +0200

    Use fallback for empty PHP_BINARY constant in cli-adapter
    Use hardcoded “php”-binary fallback in cli-adapter, because PHP_BINARY constant can be an empty string.

--- tests/Adapter/CliTest.php
+++ tests/Adapter/CliTest.php
@@ -31,4 +31,22 @@ class CliTest extends \PHPUnit\Framework\TestCase
         $code = Code::fromString('throw new \Exception("test");');
         $result = $cli->run($code);
     }
+
+    public function testPhpBinary()
+    {
+        $cli = new Cli();
+        $cli->setTempDir(sys_get_temp_dir());
+        $cli->setLogger($this->getMockBuilder('Monolog\Logger')->disableOriginalConstructor()->getMock());
+
+        $code = Code::fromString('return PHP_BINARY;');
+
+        $result = $cli->run($code);
+
+        if ('' === PHP_BINARY) {
+            $this->assertNotEmpty($result);
+            $this->assertFileExists($result);
+        } else {
+            $this->assertSame(PHP_BINARY, $result);
+        }
+    }
 }

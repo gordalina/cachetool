79eb590963f8f50e9f7fbddb3b2c49af1961418b
Serhii Varakuta <8980561+floodcode@users.noreply.github.com>
2019-12-05 22:26:24 +0200

    Implement --batch and --exclude options for opcache:compile:scripts command (#129)
    * Implement --batch and --exclude options for opcache:compile:scripts command
    
    * Fix OpcacheProxyTest
    
    * Remove .idea from .gitignore
    
    * Add test for opcache_compile_files()

--- tests/Proxy/OpcacheProxyTest.php
+++ tests/Proxy/OpcacheProxyTest.php
@@ -6,12 +6,25 @@ class OpcacheProxyTest extends ProxyTest
 {
     public function testGetFunctions()
     {
-        $this->assertCount(6, $this->createProxyInstance()->getFunctions());
+        $this->assertCount(7, $this->createProxyInstance()->getFunctions());
     }
 
     public function testFunctions()
     {
         $this->assertProxyCode("return opcache_compile_file('file');", 'opcache_compile_file', ['file']);
+        $this->assertProxyCode(join(PHP_EOL, [
+            '$paths = [',
+            "    'file1',",
+            "    'file2',",
+            '];',
+            'foreach ($paths as $path) {',
+            '    $compiled = opcache_compile_file($path);',
+            '    if (!$compiled) {',
+            '        return false;',
+            '    }',
+            '}',
+            'return true;'
+        ]), 'opcache_compile_files', [['file1', 'file2']]);
         $this->assertProxyCode("return opcache_get_configuration();", 'opcache_get_configuration', []);
         $this->assertProxyCode("return opcache_get_status(true);", 'opcache_get_status', [true]);
         $this->assertProxyCode("return opcache_invalidate('key', true);", 'opcache_invalidate', ['key', true]);
